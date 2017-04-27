a3180ee4c82343fe3f065d3475780fbfa8f5b373
Dan Ziv <dan.ziv@kaltura.com>
2017-04-27 17:10:23 +0300

    init impl

--- test/src/hls.spec.js
+++ test/src/hls.spec.js
@@ -1,4 +1,39 @@
-describe('hls', function () {
-  it('', () => {
+import playkit from 'playkit-js';
+import HlsAdapter from '../../src/hls.js'
+
+describe('HlsAdapter', function () {
+  this.timeout(4000);
+
+  it('should play hls stream - preload none', (done) => {
+    let player = playkit({
+      sources: [{
+        mimetype: "application/x-mpegurl",
+        url: "http://www.streambox.fr/playlists/test_001/stream.m3u8"
+      }]
+    });
+    let video = document.getElementsByTagName("video")[0];
+    video.onplaying = function () {
+      player.destroy();
+      done();
+    };
+    player.load();
+    player.play();
+  });
+
+  it('should play hls stream - preload none', (done) => {
+    let player = playkit({
+      preload: "auto",
+      sources: [{
+        mimetype: "application/x-mpegurl",
+        url: "http://www.streambox.fr/playlists/test_001/stream.m3u8"
+      }]
+    });
+    let video = document.getElementsByTagName("video")[0];
+    video.onplaying = function () {
+      player.destroy();
+      done();
+    };
+    player.load();
+    player.play();
   });
 });
