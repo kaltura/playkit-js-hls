e2d82d205f3dc5bd3d146f650bda4cbe4a5bf157
Dan Ziv <dan.ziv@kaltura.com>
2017-06-06 15:50:19 +0300

    delete jsdoc for code review (will be reverted after)

--- src/hls-adapter.js
+++ src/hls-adapter.js
@@ -11,26 +11,9 @@ import {CustomEvents} from 'playkit-js'
  * @classdesc
  */
 export default class HlsAdapter extends FakeEventTarget implements IMediaSourceAdapter {
-  /**
-   * The name of the adapter.
-   * @member {string} _name
-   * @static
-   * @private
-   */
+
   static _name: string = 'HlsAdapter';
-  /**
-   * The adapter logger.
-   * @member {any} _logger
-   * @static
-   * @private
-   */
   static _logger = LoggerFactory.getLogger(HlsAdapter._name);
-  /**
-   * The supported mime types by the hls adapter.
-   * @member {Array<string>} _hlsMimeType
-   * @static
-   * @private
-   */
   static _hlsMimeTypes: Array<string> = [
     'application/x-mpegurl',
     'application/vnd.apple.mpegurl',
@@ -40,49 +23,15 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     'video/mpegurl',
     'application/mpegurl'
   ];
-  /**
-   * The adapter config.
-   * @member {Object} _config
-   * @private
-   */
+
   _config: Object;
-  /**
-   * The video element.
-   * @member {HTMLVideoElement} _videoElement
-   * @private
-   */
   _videoElement: HTMLVideoElement;
-  /**
-   * The source object.
-   * @member {Source} _sourceObj
-   * @private
-   */
   _sourceObj: ?Source;
-  /**
-   * The hls player instance.
-   * @member {any} _hls
-   * @private
-   */
   _hls: any;
-  /**
-   * The load promise
-   * @member {Promise<Object>} - _loadPromise
-   * @type {Promise<Object>}
-   * @private
-   */
   _loadPromise: ?Promise<Object>;
-  /**
-   * Reference to the player tracks.
-   * @member {Array<Track>} - _playerTracks
-   * @type {Array<Track>}
-   * @private
-   */
   _playerTracks: Array<Track>;
 
-  /**
-   * Getter for the adapter name.
-   * @returns {string} - The adapter name.
-   */
+
   static get name(): string {
     return HlsAdapter._name;
   }
@@ -91,51 +40,23 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     // Do nothing. Just a workaround for flow issue with static getter in an inheritor. See: https://github.com/facebook/flow/issues/3008.
   }
 
-  /**
-   * Checks if hls adapter can play a given mime type.
-   * @function canPlayType
-   * @param {string} mimeType - The mime type to check.
-   * @returns {boolean} - Whether the hls adapter can play a specific mime type.
-   * @static
-   */
   static canPlayType(mimeType: string): boolean {
     let canHlsPlayType = HlsAdapter._hlsMimeTypes.includes(mimeType);
     HlsAdapter._logger.debug('canPlayType result for mimeType:' + mimeType + ' is ' + canHlsPlayType.toString());
     return canHlsPlayType;
   }
 
-  /**
-   * Factory method to create media source adapter.
-   * @function createAdapter
-   * @param {HTMLVideoElement} videoElement - The video element which will bind to the hls adapter.
-   * @param {Source} source - The source Object.
-   * @param {Object} config - The media source adapter configuration.
-   * @returns {IMediaSourceAdapter} - New instance of the run time media source adapter.
-   * @static
-   */
   static createAdapter(videoElement: HTMLVideoElement, source: Source, config: Object): IMediaSourceAdapter {
     HlsAdapter._logger.debug('Creating adapter. Hls version: ' + Hlsjs.version);
     return new this(videoElement, source, config);
   }
 
-  /**
-   * Checks if the hls adapter is supported.
-   * @function isSupported
-   * @returns {boolean} - Whether hls is supported.
-   * @static
-   */
   static isSupported(): boolean {
     let isHlsSupported = Hlsjs.isSupported();
     HlsAdapter._logger.debug('isSupported:' + isHlsSupported);
     return isHlsSupported;
   }
 
-  /**
-   * @constructor
-   * @param {HTMLVideoElement} videoElement - The video element which will bind to the hls adapter
-   * @param {Object} source - The source object
-   * @param {Object} config - The media source adapter configuration
-   */
   constructor(videoElement: HTMLVideoElement, source: Source, config: Object) {
     super();
     this._videoElement = videoElement;
@@ -144,12 +65,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     this._hls = new Hlsjs(this._config);
   }
 
-  /**
-   * Load the video source
-   * @function load
-   * @returns {Promise<Object>} - The loaded data
-   * @override
-   */
   load(): Promise<Object> {
     if (!this._loadPromise) {
       this._loadPromise = new Promise((resolve) => {
@@ -166,11 +81,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     return this._loadPromise;
   }
 
-  /**
-   * Destroying the _msPlayer
-   * @function destroy
-   * @override
-   */
   destroy(): void {
     HlsAdapter._logger.debug('destroy');
     this._loadPromise = null;
@@ -179,12 +89,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     this._hls.destroy();
   }
 
-  /**
-   * Parse the hls tracks into player tracks.
-   * @param {any} data - The event data.
-   * @returns {Array<Track>} - The parsed tracks.
-   * @private
-   */
   _parseTracks(data: any): Array<Track> {
     let audioTracks = this._parseAudioTracks(data.audioTracks || []);
     let videoTracks = this._parseVideoTracks(data.levels || []);
@@ -192,12 +96,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     return audioTracks.concat(videoTracks).concat(textTracks);
   }
 
-  /**
-   * Parse hls audio tracks into player audio tracks.
-   * @param {Array<Object>} hlsAudioTracks - The hls audio tracks.
-   * @returns {Array<AudioTrack>} - The parsed audio tracks.
-   * @private
-   */
   _parseAudioTracks(hlsAudioTracks: Array<Object>): Array<AudioTrack> {
     let audioTracks = [];
     for (let i = 0; i < hlsAudioTracks.length; i++) {
@@ -214,12 +112,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     return audioTracks;
   }
 
-  /**
-   * Parse hls video tracks into player video tracks.
-   * @param {Array<Object>} hlsVideoTracks - The hls video tracks.
-   * @returns {Array<VideoTrack>} - The parsed video tracks.
-   * @private
-   */
   _parseVideoTracks(hlsVideoTracks: Array<Object>): Array<VideoTrack> {
     let videoTracks = [];
     for (let i = 0; i < hlsVideoTracks.length; i++) {
@@ -236,12 +128,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     return videoTracks;
   }
 
-  /**
-   * Parse native video tag text tracks into player text tracks.
-   * @param {TextTrackList} vidTextTracks - The native video tag text tracks.
-   * @returns {Array<TextTrack>} - The parsed text tracks.
-   * @private
-   */
   _parseTextTracks(vidTextTracks: TextTrackList): Array<TextTrack> {
     let textTracks = [];
     for (let i = 0; i < vidTextTracks.length; i++) {
@@ -259,13 +145,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     return textTracks;
   }
 
-  /**
-   * Select an audio track.
-   * @function selectAudioTrack
-   * @param {AudioTrack} audioTrack - the audio track to select.
-   * @returns {boolean} - success
-   * @public
-   */
   selectAudioTrack(audioTrack: AudioTrack): boolean {
     if (audioTrack && audioTrack instanceof AudioTrack && this._hls.audioTracks) {
       this._hls.audioTrack = audioTrack.id;
@@ -274,13 +153,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     return false;
   }
 
-  /**
-   * Select a video track.
-   * @function selectVideoTrack
-   * @param {VideoTrack} videoTrack - the track to select.
-   * @returns {boolean} - success
-   * @public
-   */
   selectVideoTrack(videoTrack: VideoTrack): boolean {
     this._hls.off(Hlsjs.Events.LEVEL_SWITCHED, this._onLevelSwitched);
     if (videoTrack && videoTrack instanceof VideoTrack && this._hls.levels) {
@@ -290,13 +162,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     return false;
   }
 
-  /**
-   * Select a text track.
-   * @function selectTextTrack
-   * @param {TextTrack} textTrack - the track to select.
-   * @returns {boolean} - success
-   * @public
-   */
   selectTextTrack(textTrack: TextTrack): boolean {
     if (textTrack && textTrack instanceof TextTrack && this._videoElement.textTracks) {
       this._disableAllTextTracks();
@@ -306,26 +171,11 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     return false;
   }
 
-  /**
-   * Enables adaptive bitrate switching according to hls.js logic.
-   * @function enableAdaptiveBitrate
-   * @returns {void}
-   * @public
-   */
   enableAdaptiveBitrate(): void {
     this._hls.on(Hlsjs.Events.LEVEL_SWITCHED, this._onLevelSwitched.bind(this));
     this._hls.nextLevel = -1;
   }
 
-  /**
-   * If auto level enabled the method will extract the selected video
-   * track and trigger the 'videotrackchanged' event forward.
-   * @function _onLevelSwitched
-   * @param {string} event - The event name.
-   * @param {any} data - The event data object.
-   * @private
-   * @returns {void}
-   */
   _onLevelSwitched(event: string, data: any): void {
     if (this._hls.autoLevelEnabled) {
       let videoTrack = this._playerTracks.find((track) => {
@@ -338,11 +188,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     }
   }
 
-  /**
-   * Disables all the video tag text tracks.
-   * @returns {void}
-   * @privateh
-   */
   _disableAllTextTracks() {
     let vidTextTracks = this._videoElement.textTracks;
     for (let i = 0; i < vidTextTracks.length; i++) {
@@ -350,12 +195,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
     }
   }
 
-  /**
-   * Getter for the src that the adapter plays on the video element.
-   * In case the adapter preformed a load it will return the manifest url.
-   * @public
-   * @returns {string} - The src url.
-   */
   get src(): string {
     if (this._loadPromise != null && this._sourceObj && this._sourceObj.url) {
       return this._sourceObj.url;
@@ -364,7 +203,6 @@ export default class HlsAdapter extends FakeEventTarget implements IMediaSourceA
   }
 }
 
-// Register hls adapter to the media source adapter provider.
 if (HlsAdapter.isSupported()) {
   registerMediaSourceAdapter(HlsAdapter);
 }
