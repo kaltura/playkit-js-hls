877365fcc0ad9768f369d2320b74436d58e233b8
Dan Ziv <dan.ziv@kaltura.com>
2017-06-29 13:40:44 +0300

    chore: update dist

--- dist/playkit-hls.js
+++ dist/playkit-hls.js
@@ -103,7 +103,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 /**
- * Adapter of hls.js lib for hls content
+ * Adapter of hls.js lib for hls content.
  * @classdesc
  */
 var HlsAdapter = function (_BaseMediaSourceAdapt) {
