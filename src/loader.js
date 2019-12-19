436387c2b244cd777e79e6c5e3016e7137a2f68f
yair.ansbacher <yairans@gmail.com>
2019-12-19 14:30:07 +0200

    feat(FEC-9452): async request filter handlers

--- src/loader.js
+++ src/loader.js
@@ -16,15 +16,16 @@ export default class loader extends Hlsjs.DefaultConfig.loader {
     stats.loaded = 0;
     const xhrSetup = this.xhrSetup;
 
+    let xhrSetupPromise;
     try {
       if (xhrSetup) {
         try {
-          xhrSetup(xhr, context.url, context);
+          xhrSetupPromise = xhrSetup(xhr, context.url, context);
         } catch (e) {
           // fix xhrSetup: (xhr, url) => {xhr.setRequestHeader("Content-Language", "test");}
           // not working, as xhr.setRequestHeader expects xhr.readyState === OPEN
           xhr.open('GET', context.url, true);
-          xhrSetup(xhr, context.url, context);
+          xhrSetupPromise = xhrSetup(xhr, context.url, context);
         }
       }
       if (!xhr.readyState) {
@@ -32,20 +33,25 @@ export default class loader extends Hlsjs.DefaultConfig.loader {
       }
     } catch (e) {
       // IE11 throws an exception on xhr.open if attempting to access an HTTP resource over HTTPS
-      this.callbacks.onError({code: xhr.status, text: e.message}, context, xhr);
-      return;
+      xhrSetupPromise = Promise.reject(e);
     }
+    xhrSetupPromise = xhrSetupPromise || Promise.resolve();
+    xhrSetupPromise
+      .then(() => {
+        if (context.rangeEnd) {
+          xhr.setRequestHeader('Range', 'bytes=' + context.rangeStart + '-' + (context.rangeEnd - 1));
+        }
 
-    if (context.rangeEnd) {
-      xhr.setRequestHeader('Range', 'bytes=' + context.rangeStart + '-' + (context.rangeEnd - 1));
-    }
+        xhr.onreadystatechange = this.readystatechange.bind(this);
+        xhr.onprogress = this.loadprogress.bind(this);
+        xhr.responseType = context.responseType;
 
-    xhr.onreadystatechange = this.readystatechange.bind(this);
-    xhr.onprogress = this.loadprogress.bind(this);
-    xhr.responseType = context.responseType;
-
-    // setup timeout before we perform request
-    this.requestTimeout = window.setTimeout(this.loadtimeout.bind(this), this.config.timeout);
-    xhr.send();
+        // setup timeout before we perform request
+        this.requestTimeout = window.setTimeout(this.loadtimeout.bind(this), this.config.timeout);
+        xhr.send();
+      })
+      .catch(e => {
+        this.callbacks.onError({code: xhr.status, text: e.message}, context, xhr);
+      });
   }
 }
