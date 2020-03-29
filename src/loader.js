d57ae6cbb8d0c49e344e5dbed1ec6957ee142eeb
yair.ansbacher <yairans@gmail.com>
2020-03-29 16:24:38 +0300

    remove logger

--- src/loader.js
+++ src/loader.js
@@ -1,14 +1,11 @@
 //@flow
 import Hlsjs from 'hls.js';
-import {getLogger} from '@playkit-js/playkit-js';
 
 /**
  * A custom override for the loader function in hls.js.
  * It passes the context for the xhrSetup config
  */
 export default class loader extends Hlsjs.DefaultConfig.loader {
-  _logger = getLogger('HlsCustomLoader');
-
   constructor(config) {
     super(config);
     if (config && config.readystatechange) {
