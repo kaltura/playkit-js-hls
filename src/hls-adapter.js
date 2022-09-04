58c7ae570baf718ffed5c94002eaf1e1bdfc76e2
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2022-09-04 12:58:01 +0300

    init playerTracks so it wont be undefined before manifest loaded

--- src/hls-adapter.js
+++ src/hls-adapter.js
@@ -112,7 +112,7 @@ export default class HlsAdapter extends BaseMediaSourceAdapter {
    * @type {Array<Track>}
    * @private
    */
-  _playerTracks: Array<Track>;
+  _playerTracks: Array<Track> = [];
   /**
    * stream start time in seconds
    * @type {?number}
@@ -282,6 +282,14 @@ export default class HlsAdapter extends BaseMediaSourceAdapter {
     if (this._config.forceRedirectExternalStreams) {
       this._config.hlsConfig['pLoader'] = pLoader;
     }
+    //this._config.hlsConfig.debug = true;
+    //this._config.hlsConfig.autoStartLoad = false;
+    //this._config.hlsConfig.lowLatencyMode = true;
+    //this._config.hlsConfig.liveSyncDurationCount = 20;
+    //this._config.hlsConfig.liveDurationInfinity = true;
+    this._config.hlsConfig.startLevel = 0;
+    this._config.hlsConfig.startFragPrefetch = true;
+
     this._maybeSetFilters();
     this._hls = new Hlsjs(this._config.hlsConfig);
     this._capabilities.fpsControl = true;
