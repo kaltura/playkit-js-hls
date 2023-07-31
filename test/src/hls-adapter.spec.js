05a9a6e2129ebb0703fc12a8fc8f5ac20850b26e
Sivan Agranov <88330203+SivanA-Kaltura@users.noreply.github.com>
2023-07-31 15:25:27 +0300

    chore: revert hls.js upgrade

--- test/src/hls-adapter.spec.js
+++ test/src/hls-adapter.spec.js
@@ -970,11 +970,10 @@ describe('HlsAdapter Instance: response filter', () => {
         }
       })
     );
-
-    sandbox.spy(hlsAdapterInstance._hls.networkControllers[0], 'handleMasterPlaylist');
+    sandbox.spy(hlsAdapterInstance._hls.networkControllers[0], 'loadsuccess');
     hlsAdapterInstance._hls.on(hlsAdapterInstance._hlsjsLib.Events.MANIFEST_LOADED, () => {
       try {
-        hlsAdapterInstance._hls.networkControllers[0].handleMasterPlaylist.getCall(0).firstArg.data.indexOf('&test').should.be.gt(-1);
+        hlsAdapterInstance._hls.networkControllers[0].loadsuccess.getCall(0).firstArg.data.indexOf('&test').should.be.gt(-1);
         done();
       } catch (e) {
         done(e);
@@ -998,7 +997,7 @@ describe('HlsAdapter Instance: response filter', () => {
         }
       })
     );
-    sandbox.stub(hlsAdapterInstance._hls.networkControllers[0], 'handleMasterPlaylist').callsFake(value => {
+    sandbox.stub(hlsAdapterInstance._hls.networkControllers[0], 'loadsuccess').callsFake(value => {
       try {
         value.data.indexOf('&test').should.be.gt(-1);
         done();
